fc9d0338c93df89e51341c6ebb2bf1c0ffcc5639
Salesmeet <87336522+Salesmeet@users.noreply.github.com>
2021-10-06 22:53:01 +0200

    Add files via upload

---
+++ index_new.php
@@ -0,0 +1,30 @@
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@100;200;300;400;500&display=swap" rel="stylesheet">
+    <link  rel="stylesheet" type="text/css"  defer="defer" href="asset/main.css" />
+    <script src="https://kit.fontawesome.com/40e8a4814d.js" crossorigin="anonymous"></script>
+    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+    <!-- script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script -->
+    <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+    <link rel="stylesheet" type="text/css" href="asset/slick/slick.css">
+    <link rel="stylesheet" type="text/css" href="asset/slick/slick-theme.css">
+    <script src="asset/slick/slick.js" type="text/javascript" charset="utf-8"></script>
+  </head>
+  <body>
+
+  <?php include("section/menu.php") ?>
+  <?php include("section/intro.php") ?>
+  <?php include("section/skills.php") ?>
+  <?php include("section/vantaggi.php") ?>
+  <?php include("section/dicono.php") ?>
+  <?php include("section/aziende.php") ?>
+  <?php include("section/partners.php") ?>
+  <?php include("section/contact.php") ?>
+
+  <?php include("section/footer.php") ?>
+
+  </body>
+</html>
