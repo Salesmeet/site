79bc724036b39dafead3608835f48cdf4bc2b49f
Salesmeet <tech@salesmeet.ai>
2021-10-07 12:07:55 +0200

    contact

--- src/section/contact.php
+++ src/section/contact.php
@@ -13,23 +13,97 @@
     font-family: Heebo;
     font-style: normal;
     font-weight: bold;
-    font-size: 19px;
-    line-height: 28px;
-    text-align: center;
     text-transform: uppercase;
     padding-top: 50px;
+    font-size: 40px;
+    line-height: 69px;
+    text-align: center;
+    color: #FFFFFF;
   }
   .contact .description {
     font-family: Heebo;
     font-style: normal;
     font-weight: normal;
-    font-size: 14px;
-    line-height: 21px;
+    font-size: 15px;
+    line-height: 15px;
     text-align: center;
+    color: #FFFFFF;
   }
+  .contact .object1 {
+      position: absolute;
+      left: 35%;
+      bottom: -20%;
+      z-index: 1;
+      width: 30%;
+  }
+  .contact .object2 {
+    position: absolute;
+        bottom: 0%;
+        z-index: 2;
+        width: 50%;
+        transform: rotate(180deg);
+        left: 20%;
+  }
+  .contact .object4 {
+      position: absolute;
+      z-index: 3;
+      right: 0px;
+      bottom: -150px;
+      width: 30%;
+  }
+  .contact .buttoncontenitore {
+      text-align: center;
+      margin: 0 auto;
+  }
+
+  .contact .buttoncontenitore {
+        z-index: 9999;
+        position: relative;
+        margin-top: 40px;
+        background-color: #69A3F1;
+        width: 300px;
+        padding: 10px;
+        color: #fff;
+        border-radius: 20px;
+  }
+  .contact .buttoncontenitore a {
+        color: #fff;
+  }
+  @media (max-width: 880px) {
+    .contact .title {
+        font-size: 30px;
+        line-height: 39px;
+    }
+    .contact .object1 {
+        left: 15%;
+        bottom: 0%;
+        width: 60%;
+    }
+    .contact .object2 {
+        left: 0%;
+    }
+    .contact .object4 {
+        bottom: 0px;
+        width: 50%;
+    }
+  }
+
 </style>
 <div class="contact" id="contactusdiv">
-  <div class="title">contact</div>
-  <div class="description">Bla bla bla Bla bla bla Bla bla bla Bla bla bla Bla bla bla Bla bla bla </div>
+  <div class="title">JOIN THE BIG VOICE REVOLUTION</div>
+  <div class="description">SAME Ai is the next-gen knowledge management system</div>
+  <div class="buttoncontenitore">
+      <div class="button"><a href="mailto:info@salesmeet.ai">NEED HELP? LET’S CHAT</a></div>
+  </div>
+
+  <div class="">
+      <img class="object1" src="img/Ellipse2.png">
+      <img class="object2" src="img/Ellipse4.png">
+      <img class="object4" src="img/Ellipse3.png">
+  </div>
+
 </div>
+
+
+
 <div class="separator"></div>
